11835c16538a3ed8d1befedc13e207ce18a53419
Vikas Kumar Gupta <vikas.vg118@gmail.com>
2022-02-01 10:08:18 +0530

    css module update

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,13 +1,23 @@
+import React from 'react';
+
+import styles from './Button.module.css';
+
+/*
 import styled from "styled-components";
 
 const Button = styled.button`
   font: inherit;
+  width: 100%;
   padding: 0.5rem 1.5rem;
   border: 1px solid #8b005d;
   color: white;
   background: #8b005d;
   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
   cursor: pointer;
+
+  @media (min-width: 768px) {
+    width: auto;
+  }
 
   &:focus {
     outline: none;
@@ -21,12 +31,14 @@ const Button = styled.button`
   }
 `;
 
-// const Button = props => {
-//   return (
-//     <button type={props.type} className="button" onClick={props.onClick}>
-//       {props.children}
-//     </button>
-//   );
-// };
+*/
+
+const Button = props => {
+  return (
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
 
 export default Button;
